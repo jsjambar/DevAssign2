c2b6d525ff46519147c0846f8695cb96c751ed41
Andy Bhadai <abhadai@gmail.com>
2016-10-21 16:19:07 +0200

    Created a methods to create and populate a database

--- Assignment2/Assignment2/Program.cs
+++ Assignment2/Assignment2/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +12,49 @@ namespace Assignment2
 {
     class Program
     {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _database;
+
         static void Main(string[] args)
         {
+            CreateAndPopulateDatabase();
+            Console.ReadLine();
+        }
+
+        // Maybe split into two methods? 
+        public static async void CreateAndPopulateDatabase()
+        {
+            _client = new MongoClient();
+            _database = _client.GetDatabase("assignment2");
+
+            var document = new BsonDocument
+            {
+                { "employees" , new BsonArray
+                    {
+                        new BsonDocument
+                        {
+                            { "bsn", "1234" },
+                            { "first_name", "Andy" },
+                            { "surname", "Bhadai" }
+                        },
+                        new BsonDocument
+                        {
+                            { "bsn", "5678" },
+                            { "first_name", "Jason" },
+                            { "surname", "Sjambar" }
+                        },
+                        new BsonDocument
+                        {
+                            { "bsn", "0001" },
+                            { "first_name", "Pietje" },
+                            { "surname", "Puk" }
+                        }
+                    }
+                },
+            };
+
+            var collection = _database.GetCollection<BsonDocument>("restaurants");
+            await collection.InsertOneAsync(document);
         }
     }
 }
